873538bc3f9bf4395822a01bfb25880460c0b01d
lincolnyuji <lincolnyuji@hotmail.com>
2024-04-04 16:41:51 -0300

    Move mock data to a proper JSON file

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,29 +1,15 @@
 import { useParams } from "react-router-dom";
+import data from '../data/games.json';
+
+import '../css/Game.css'
 
 const Game = () => {
     const { id } = useParams();
 
-    const game1 = {
-        id: 0,
-        name: "Cut the Rope",
-        url: "https://play.famobi.com/wrapper/cut-the-rope"
-    }
-
-    const game2 = {
-        id: 1,
-        name: "Cursed Marbles",
-        url: "https://play.famobi.com/totemia-cursed-marbles"
-    }
-
-    const games = [
-        game1,
-        game2
-    ];
-
     return (
         <>
-            <h1>{games[id].name}</h1>
-            <iframe src={games[id].url} width="1280" height="720" frameborder="no" scrolling="no"></iframe>
+            <h1 className="game-title">{data[id].name}</h1>
+            <iframe src={data[id].url} width="1080" height="720" frameborder="no" className="game-frame"></iframe>
         </>
     )
 }
