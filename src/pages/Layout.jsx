1e976c6e0c160f4050f5aacc9fe4bfad5146d74f
lincolnyuji <lincolnyuji@hotmail.com>
2024-04-05 10:28:05 -0300

    Add base CSS style to Navigation Bar

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,14 +1,24 @@
 import { Outlet, Link } from "react-router-dom";
 
+import '../css/Layout.css'
+
 const Layout = () => {
   return (
     <>
       <nav>
-          <button>Categories</button>
-          <Link to="/">Home</Link>
-          <Link to="/contact">Contact</Link>
-          <input type="text" placeholder="Pesquisa"/>
+        <div className="nav-container">
+          <div className="nav-content">
+            <button id="side-toggle">Categorias</button>
+          </div>
+          <div className="nav-home">
+            <Link to="/" className="nav-page">Home</Link>
+          </div>
+          <div className="nav-content">
+            <input type="text" placeholder=" Pesquisa"/>
+          </div>
+        </div>
       </nav>
+      <Link to="/contact">CRUD Test</Link>
       <Outlet />
     </>
   )
